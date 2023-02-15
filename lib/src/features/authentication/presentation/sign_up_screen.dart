ded009dedd9cf78dab3e0cb3f6b43566c63bbcce
Miqdad <miqdaddeveloper@gmail.com>
2023-02-15 22:46:13 +0700

    (feat) Add SignUpScreen

---
+++ lib/src/features/authentication/presentation/sign_up_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SignUpScreen extends StatelessWidget {
+  static const String routeName = '/sign-up';
+  const SignUpScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
