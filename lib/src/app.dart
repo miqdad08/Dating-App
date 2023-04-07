f171dd854fa33b92839ff72512913199ce9c206b
Miqdad <miqdaddeveloper@gmail.com>
2023-04-07 13:17:40 +0700

    (feat & fix) Add initialRoutes if IsRegiste = true, Add Some Route

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,21 +1,53 @@
+import 'package:dating_app/src/features/authentication/data/data_user_account_local.dart';
 import 'package:dating_app/src/features/authentication/presentation/sign_up_screen.dart';
+import 'package:dating_app/src/features/authentication/presentation/sign_up_upload_photo_screen.dart';
+import 'package:dating_app/src/features/likes_you/presentation/explore_people_screen.dart';
+import 'package:dating_app/src/features/likes_you/presentation/people_loved_screen.dart';
+import 'package:dating_app/src/features/likes_you/presentation/people_profile_screen.dart';
 import 'package:flutter/material.dart';
 
 import 'theme_manager/theme_data_manager.dart';
 
-class AppScreen extends StatelessWidget {
+class AppScreen extends StatefulWidget {
   const AppScreen({
     Key? key,
   }) : super(key: key);
+
+  @override
+  State<AppScreen> createState() => _AppScreenState();
+}
+
+class _AppScreenState extends State<AppScreen> {
+  bool isRegister = false;
+
+  @override
+  void initState() {
+    super.initState();
+    isUserRegister();
+  }
+
+  isUserRegister() async {
+    isRegister = await UserAccountRegister.getUserAccountRegister();
+    print(isRegister);
+    print('qeq');
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: getApplicationThemeData(),
-      initialRoute: SignUpScreen.routeName,
+      home: isRegister ? ExplorePeopleScreen() : SignUpScreen(),
+      // initialRoute: SignUpScreen.routeName,
       routes: {
         SignUpScreen.routeName: (context) => const SignUpScreen(),
+        // SignUpAgeJobScreen.routeName: (context) => const SignUpAgeJobScreen(),
+        SignUpUploadPhotoScreen.routeName: (context) =>
+            const SignUpUploadPhotoScreen(),
+        ExplorePeopleScreen.routeName: (context) => const ExplorePeopleScreen(),
+        PeopleLovedScreen.routeName: (context) => const PeopleLovedScreen(),
+        PeopleProfileScreen.routeName: (context) => const PeopleProfileScreen(),
       },
     );
   }
