b457ee876a44ccda515713c6ed1b082aac662219
Miqdad <miqdaddeveloper@gmail.com>
2023-04-07 13:30:01 +0700

    (feat) Add Some Const Of Size

--- lib/src/theme_manager/values_manager.dart
+++ lib/src/theme_manager/values_manager.dart
@@ -8,6 +8,7 @@ class AppMargin {
   static const double m20 = 20.0;
   static const double m22 = 22.0;
   static const double m24 = 24.0;
+  static const double m26 = 26.0;
   static const double m28 = 28.0;
   static const double m30 = 30.0;
   static const double m32 = 32.0;
@@ -20,6 +21,7 @@ class AppPadding {
   static const double p2 = 2.0;
   static const double p4 = 4.0;
   static const double p8 = 8.0;
+  static const double p10 = 10.0;
   static const double p12 = 12.0;
   static const double p15 = 15.0;
   static const double p16 = 16.0;
@@ -27,6 +29,7 @@ class AppPadding {
   static const double p20 = 20.0;
   static const double p22 = 22.0;
   static const double p24 = 24.0;
+  static const double p26 = 26.0;
   static const double p28 = 28.0;
   static const double p30 = 30.0;
   static const double p32 = 32.0;
@@ -53,4 +56,5 @@ class AppSize {
   static const double s36 = 36.0;
   static const double s40 = 40.0;
   static const double s50 = 50.0;
+  static const double s70 = 70.0;
 }
