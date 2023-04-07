0756b585b12eae601e3e2deb7eccb07a06f5ba47
Miqdad <miqdaddeveloper@gmail.com>
2023-04-07 13:26:00 +0700

    (feat) Add PeopleLovedEvent

---
+++ lib/src/features/likes_you/presentation/bloc/people_loved/people_loved_event.dart
@@ -0,0 +1,11 @@
+part of 'people_loved_bloc.dart';
+
+@immutable
+abstract class PeopleLovedEvent {}
+
+class OnPeopleLovedEventCalled extends PeopleLovedEvent{}
+class AddPeopleLoved extends PeopleLovedEvent{
+  final User user;
+
+  AddPeopleLoved({required this.user});
+}
