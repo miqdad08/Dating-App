7e146c594a529d1801e1fd3fd27e86cc95edb59c
Miqdad <miqdaddeveloper@gmail.com>
2023-04-07 13:29:40 +0700

    (feat) Add UserAccountModel

---
+++ lib/src/features/authentication/domain/user_account.dart
@@ -0,0 +1,71 @@
+import 'dart:convert';
+
+class UserAccount {
+  final String fullName;
+  final String email;
+  final String password;
+  final String occupation;
+  final String age;
+  String? imageProfile;
+
+  UserAccount({
+    required this.fullName,
+    required this.email,
+    required this.password,
+    required this.occupation,
+    required this.age,
+    this.imageProfile,
+  });
+
+  @override
+  String toString() {
+    return 'UserAccout(fullName: $fullName, email: $email, password: $password, occupation: $occupation, age: $age, imageProfile: $imageProfile)';
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'fullName': fullName,
+      'email': email,
+      'password': password,
+      'occupation': occupation,
+      'age': age,
+      'imageProfile': imageProfile,
+    };
+  }
+
+  factory UserAccount.fromMap(Map<String, dynamic> map) {
+    return UserAccount(
+      fullName: map['fullName'],
+      email: map['email'] as String,
+      password: map['password'] as String,
+      occupation: map['occupation'] as String,
+      age: map['age'] as String,
+      imageProfile:
+          map['imageProfile'] != null ? map['imageProfile'] as String : null,
+    );
+  }
+
+  factory UserAccount.fromJson(String source) =>
+      UserAccount.fromMap(json.decode(source));
+
+  @override
+  bool operator ==(covariant UserAccount other) {
+    if (identical(this, other)) return true;
+    return other.fullName == fullName &&
+        other.email == email &&
+        other.password == password &&
+        other.occupation == occupation &&
+        other.age == age &&
+        other.imageProfile == imageProfile;
+  }
+
+  @override
+  int get hashCode {
+    return fullName.hashCode ^
+        email.hashCode ^
+        password.hashCode ^
+        occupation.hashCode ^
+        age.hashCode ^
+        imageProfile.hashCode;
+  }
+}
