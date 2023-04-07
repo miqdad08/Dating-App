8b445101d80d15c7fcb1e28321cc1097fa922d38
Miqdad <miqdaddeveloper@gmail.com>
2023-04-07 13:19:06 +0700

    (feat) Add Auth Bloc & Set Data User

--- lib/src/features/authentication/presentation/bloc/auth_bloc.dart
+++ lib/src/features/authentication/presentation/bloc/auth_bloc.dart
@@ -1,15 +1,26 @@
-
 import 'package:bloc/bloc.dart';
+import 'package:dating_app/src/features/authentication/data/data_user_account_local.dart';
 import 'package:meta/meta.dart';
 
 import '../../domain/user_account.dart';
 
 part 'auth_event.dart';
+
 part 'auth_state.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
   AuthBloc() : super(AuthInitial()) {
-    on<AuthEvent>((event, emit) {
+    on<RegisterAuthEvent>((event, emit) async {
+      emit(AuthLoading());
+      DataUserAccountLocal.setDataUserAccountToStorage(event.userAccount);
+      UserAccountRegister.setUserAccountRegister(event.isRegister);
+      await Future.delayed(const Duration(seconds: 2));
+      emit(
+        AuthSuccess(
+          userAccount: event.userAccount,
+          isRegister: event.isRegister,
+        ),
+      );
     });
   }
 }
